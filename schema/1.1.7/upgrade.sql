e908c1a62bc60e6172a81388cd0e5fa2609009c2
Tiago Bember Simeao <tbembersimeao@ufl.edu>
2018-04-23 12:55:29 -0400

    Adding upgrade.sql to 1.1.7 schema.

---
+++ schema/1.1.7/upgrade.sql
@@ -0,0 +1,11 @@
+ALTER TABLE entries CHANGE `07_count_drinks` `07_count_drinks` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_set_number_drinks` `07_set_number_drinks` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_eat_before` `07_eat_before` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_space_drinks_out` `07_space_drinks_out` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_alternate_drinks` `07_alternate_drinks` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_drink_for_quality` `07_drink_for_quality` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_avoid_drinking_games` `07_avoid_drinking_games` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_have_a_reliable_driver` `07_have_a_reliable_driver` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_preplan_transportation` `07_preplan_transportation` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_dst_protection` `07_dst_protection` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
+ALTER TABLE entries CHANGE `07_watch_out_for_each_other` `07_watch_out_for_each_other` ENUM('never','almost-never','some-time','half-time','most-time','almost-always','always','skip') DEFAULT NULL;
